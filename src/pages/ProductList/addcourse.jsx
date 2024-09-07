a610f27a62c8bacd0e6c31ff5767619856001e2d
TechnerdAcademy <technerdacademy111@gmail.com>
2024-09-07 19:23:49 +0530

    Update addcourse.jsx

--- src/pages/ProductList/addcourse.jsx
+++ src/pages/ProductList/addcourse.jsx
@@ -35,7 +35,7 @@ const AddCourses = () => {
     formData.append("file", imageFile);
 
     try {
-        const { data } = await axios.post("http://localhost:4001/v1/upload", formData, {
+        const { data } = await axios.post("https://tecknerdacademy.in/api/v1/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
           "Authorization": `Bearer ${token}`, 
